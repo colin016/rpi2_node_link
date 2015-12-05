eec1ac6a015eda5ffab1737d1bce1c065f97a985
huangyao <huangyaobyte@gmail.com>
2015-12-05 01:27:01 +0000

    build

--- sensors/dht11/build/Release/.deps/Release/obj.target/dht11/dht11.o.d
+++ sensors/dht11/build/Release/.deps/Release/obj.target/dht11/dht11.o.d
@@ -1,13 +1,13 @@
-cmd_Release/obj.target/dht11/dht11.o := g++ '-DNODE_GYP_MODULE_NAME=dht11' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/0.12.6/include/node -I/home/pi/.node-gyp/0.12.6/src -I/home/pi/.node-gyp/0.12.6/deps/uv/include -I/home/pi/.node-gyp/0.12.6/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -ffunction-sections -fdata-sections -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/dht11/dht11.o.d.raw  -c -o Release/obj.target/dht11/dht11.o ../dht11.cc
+cmd_Release/obj.target/dht11/dht11.o := g++ '-DNODE_GYP_MODULE_NAME=dht11' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/4.2.1/include/node -I/home/pi/.node-gyp/4.2.1/src -I/home/pi/.node-gyp/4.2.1/deps/uv/include -I/home/pi/.node-gyp/4.2.1/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/dht11/dht11.o.d.raw   -c -o Release/obj.target/dht11/dht11.o ../dht11.cc
 Release/obj.target/dht11/dht11.o: ../dht11.cc \
- /home/pi/.node-gyp/0.12.6/src/node.h \
- /home/pi/.node-gyp/0.12.6/deps/v8/include/v8.h \
- /home/pi/.node-gyp/0.12.6/deps/v8/include/v8stdint.h \
- /home/pi/.node-gyp/0.12.6/deps/v8/include/v8config.h \
- /home/pi/.node-gyp/0.12.6/src/node_version.h
+ /home/pi/.node-gyp/4.2.1/include/node/node.h \
+ /home/pi/.node-gyp/4.2.1/include/node/v8.h \
+ /home/pi/.node-gyp/4.2.1/include/node/v8-version.h \
+ /home/pi/.node-gyp/4.2.1/include/node/v8config.h \
+ /home/pi/.node-gyp/4.2.1/include/node/node_version.h
 ../dht11.cc:
-/home/pi/.node-gyp/0.12.6/src/node.h:
-/home/pi/.node-gyp/0.12.6/deps/v8/include/v8.h:
-/home/pi/.node-gyp/0.12.6/deps/v8/include/v8stdint.h:
-/home/pi/.node-gyp/0.12.6/deps/v8/include/v8config.h:
-/home/pi/.node-gyp/0.12.6/src/node_version.h:
+/home/pi/.node-gyp/4.2.1/include/node/node.h:
+/home/pi/.node-gyp/4.2.1/include/node/v8.h:
+/home/pi/.node-gyp/4.2.1/include/node/v8-version.h:
+/home/pi/.node-gyp/4.2.1/include/node/v8config.h:
+/home/pi/.node-gyp/4.2.1/include/node/node_version.h:
